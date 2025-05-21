a3aedf62e8e426a0a60976b902f7753be6195689
eswaldots <aaronvendedor@gmail.com>
2025-05-20 20:38:39 -0400

    feat: changed to nvchad

--- .config/nvim/lua/config/keymaps.lua
+++
@@ -1,9 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
-local map = LazyVim.safe_keymap_set
-local unmap = vim.keymap.del
-
-map("n", "-", ":Oil<cr>")
-map("n", "<C-p>", "<cmd>FzfLua files<cr>", { desc = "Search files" })
